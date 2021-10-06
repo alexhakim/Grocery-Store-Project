19f15f1367f84f21cb4a3b46d857560ba7d550f5
Alexandre Hakim <hakimalexandre141@gmail.com>
2021-10-06 17:38:28 -0400

    Initial Setup

---
+++ app/src/main/java/com/coen390team10/GSAAPP/MainActivity.java
@@ -0,0 +1,14 @@
+package com.coen390team10.GSAAPP;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
