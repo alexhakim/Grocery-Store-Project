22689f48779c9eb2525dbd27d8a10f6b10818d09
Alexandre Hakim <hakimalexandre141@gmail.com>
2021-10-16 20:44:07 -0400

    Login/Signup update 1.32

--- app/src/main/java/com/coen390team10/GSAAPP/DBHelper.java
+++ app/src/main/java/com/coen390team10/GSAAPP/DBHelper.java
@@ -9,30 +9,34 @@ import androidx.annotation.Nullable;
 
 public class DBHelper extends SQLiteOpenHelper {
 
+    public static final String TABLE_USERS = "users";
+    public static final String COLUMN_USERNAME = "username";
+    public static final String COLUMN_PASSWORD = "password";
+
     public DBHelper(@Nullable Context context) {
         super(context, "GROCERYDATABASE1.3", null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (ID INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)");
+        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_USERS + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_USERNAME + " TEXT, " + COLUMN_PASSWORD + " TEXT)");
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS users");
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_USERS);
         onCreate(sqLiteDatabase);
     }
 
     public boolean registerUser(String username, String password){ // sign up
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put("username",username);
-        contentValues.put("password",password);
+        contentValues.put(COLUMN_USERNAME,username);
+        contentValues.put(COLUMN_PASSWORD,password);
 
-        long result = sqLiteDatabase.insert("users", null, contentValues);
-        return result == -1 ? false : true;
+        long result = sqLiteDatabase.insert(TABLE_USERS, null, contentValues);
+        return result != -1;
 
 
     }
@@ -40,17 +44,17 @@ public class DBHelper extends SQLiteOpenHelper {
     public boolean checkUser(String username, String password){ // sign in
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
         String[] columns = {"ID"};
-        String s = "username =? and password =?";
+        String s = COLUMN_USERNAME + " =? and " + COLUMN_PASSWORD + " =?";
         String[] sInfo = {username,password};
-        Cursor c = sqLiteDatabase.query("users",columns,s,sInfo,null,null,null);
+        Cursor c = sqLiteDatabase.query(TABLE_USERS,columns,s,sInfo,null,null,null);
         int count = c.getCount();
         sqLiteDatabase.close();
         c.close();
-        return count > 0 ? true : false;
+        return count > 0;
     }
 
     public boolean isUsernameExists(String username){ // check if user already exists in database
-        String query = "SELECT * FROM users WHERE username =? ";
+        String query = "SELECT * FROM " + TABLE_USERS + " WHERE " + COLUMN_USERNAME + " =? ";
         String[] sA = {username};
         SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
         Cursor c = sqLiteDatabase.rawQuery(query,sA);
