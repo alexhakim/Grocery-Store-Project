0acf597affee63c39acdbbaf783eedbab100a407
Alexandre Hakim <hakimalexandre141@gmail.com>
2021-10-19 00:44:25 -0400

    Pairing activity basic layout and permissions

--- app/src/main/java/com/coen390team10/GSAAPP/AfterLogin.java
+++ app/src/main/java/com/coen390team10/GSAAPP/AfterLogin.java
@@ -3,11 +3,33 @@ package com.coen390team10.GSAAPP;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class AfterLogin extends AppCompatActivity {
+
+    // TEST BLUETOOTH ACTIVITY
+    // permissions added in AndroidManifest
+
+    // need to setup database for each user
+
+    TextView connectionStatusTextView;
+    ListView deviceListView;
+    Button searchButton;
+    BluetoothAdapter bluetoothAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +40,16 @@ public class AfterLogin extends AppCompatActivity {
         // changing color of action bar
         ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#344398"));
         actionBar.setBackgroundDrawable(colorDrawable);
+
+        connectionStatusTextView = (TextView) findViewById(R.id.connectionStatusTextView);
+        deviceListView = (ListView) findViewById(R.id.deviceListView);
+        searchButton = (Button) findViewById(R.id.searchButton);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+
     }
+
+
+
 }
