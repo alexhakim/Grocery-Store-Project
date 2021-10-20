4ebd20d2ae62d68ec71b537d0bad15a4ee4cfd5a
Alexandre Hakim <hakimalexandre141@gmail.com>
2021-10-19 22:57:51 -0400

    Added UI features

---
+++ app/src/main/java/com/coen390team10/GSAAPP/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.coen390team10.GSAAPP;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    @SuppressWarnings("DEPRECATION")
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // set full screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+            }
+
+        }, 500);
+
+    }
+}
