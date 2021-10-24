04fde8bfe16d985277aa7088b3dd87ff277c9cf0
Alexandre Hakim <hakimalexandre141@gmail.com>
2021-10-24 00:15:54 -0400

    Added Checkout

---
+++ app/src/main/java/com/coen390team11/GSAAPP/CheckoutActivity.java
@@ -0,0 +1,39 @@
+package com.coen390team11.GSAAPP;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class CheckoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout);
+
+        ActionBar actionBar = getSupportActionBar();
+        // changing color of action bar
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#344398"));
+        actionBar.setBackgroundDrawable(colorDrawable);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/coen390team11/GSAAPP/ui/dashboard/BagFragment.java
+++ app/src/main/java/com/coen390team11/GSAAPP/ui/dashboard/BagFragment.java
@@ -1,5 +1,6 @@
 package com.coen390team11.GSAAPP.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,12 +13,16 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.coen390team11.GSAAPP.CheckoutActivity;
+import com.coen390team11.GSAAPP.R;
 import com.coen390team11.GSAAPP.databinding.FragmentBagBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class BagFragment extends Fragment {
 
     private BagViewModel dashboardViewModel;
     private FragmentBagBinding binding;
+    FloatingActionButton fab;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,6 +31,17 @@ public class BagFragment extends Fragment {
 
         binding = FragmentBagBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        fab = binding.fab;
+        fab.setImageResource(R.drawable.ic_baseline_shopping_cart_24);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // go to checkout
+                Intent goToCheckoutIntent = new Intent(getContext(), CheckoutActivity.class);
+                startActivity(goToCheckoutIntent);
+            }
+        });
 
 
 
