aa5698fe61c80af146b227aea64919cba7bd4141
Alexandre Hakim <hakimalexandre141@gmail.com>
2021-10-24 17:35:00 -0400

    Implemented barcode checkout feature

--- app/src/main/java/com/coen390team11/GSAAPP/CheckoutActivity.java
+++ app/src/main/java/com/coen390team11/GSAAPP/CheckoutActivity.java
@@ -4,12 +4,28 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class CheckoutActivity extends AppCompatActivity {
+
+    EditText randomEditText;
+    private ImageView barcode;
+    Button tempButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +38,40 @@ public class CheckoutActivity extends AppCompatActivity {
         actionBar.setBackgroundDrawable(colorDrawable);
         actionBar.setDisplayHomeAsUpEnabled(true);
 
+        barcode = findViewById(R.id.barcode);
+        randomEditText = findViewById(R.id.randomEditText);
+        tempButton = findViewById(R.id.tempButton);
 
+        tempButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getBarcode();
+            }
+        });
+
+
+    }
+
+    private void getBarcode(){
+        try{
+            setBarcode();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+    public void setBarcode() throws WriterException {
+
+        BitMatrix bitMatrix = multiFormatWriter.encode(randomEditText.getText().toString(), BarcodeFormat.CODE_128,400,170,null);
+        // first variable in .encode is what we want to receive, means the total price from
+        // bag fragment, for now it is set to random edit text
+
+        BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+        Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+
+        barcode.setImageBitmap(bitmap);
+        // set generated barcode (bitmap) to image view (barcode)
     }
 
     @Override
